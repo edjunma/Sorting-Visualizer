6d5c090dcb5cf06abef25850772ea0b9a8ba76b2
edjunma <edjunma@gmail.com>
2020-05-26 01:16:46 -0400

    add test sorting algorithms method

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -36,6 +36,19 @@ export default class SortingVisualizer extends React.Component {
 
 	bubbleSort() {}
 
+	testSortingAlgorithms() {
+		for (let i = 0; i < 100; i++) {
+			const array = [];
+			const length = randomIntFromInterval(1, 1000);
+			for (let i = 0; i < length; i++) {
+				array.push(randomIntFromInterval(-1000, 1000));
+			}
+			const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
+			const mergeSortedArray = sortingAlgorithms.mergeSort(array.slice());
+			console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray));
+		}
+	}
+
 	render() {
 		const { array } = this.state;
 
